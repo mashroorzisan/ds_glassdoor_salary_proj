5d4c31b15831cf27bdc209746362ca270f708f8a
zisan <mashroorzisan@gmail.com>
2023-06-05 01:16:37 +0600

    first commit

---
+++ actual runner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 01:13:22 2023
+
+@author: Mashroor
+"""
+
+import ds_scraper as gs
+import pandas as pd
+
+df = gs("data scientist", 5, False, path, 15)
+df
